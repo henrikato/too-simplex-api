7a0f495af18a0b7d55db7fb08d012d197c5f6c45
Henrique T. Kato <henrique.kato@etec.sp.gov.br>
2019-04-17 21:38:58 -0300

    Faltou o .babelrc

--- start.js
+++ start.js
@@ -1,19 +1,5 @@
 require('babel-register')({
-    presets: ['node8'],
-    /*plugins: [
-        [
-            require("babel-plugin-module-resolver"),
-            {
-                "root": "./",
-                "alias": {
-                    "@models": "./src/app/models",
-                    "@controllers": "./src/controllers",
-                    "@routes": "./src/routes",
-                    "@repositories": "./src/repositories"
-                }
-            }
-        ]
-    ]*/
+    presets: ['node8']
 })
 
 module.exports = require("./server.js");
